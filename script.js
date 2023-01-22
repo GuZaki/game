af2937b84b1e8a70cd241c553cc4f181d1316412
Guzel <viproo@bk.ru>
2023-01-22 10:29:04 +0300

    Modified script.js

--- script.js
+++ script.js
@@ -1,35 +1,28 @@
 'use strict';
 
-// const block = document.createElement('div') // создает элемент div
-// block.classList.add('block') // добавляет класс в список классов для элемента
-// document.body.append(block) //добавляем элемент block в конец body(на страницу)
-// console.dir(block) //выводит в консоль все атрибуты элемента
-// block.innerText = 'Hello'// передает строку в элемент
-// block.innerHTML = '<h1>Hello</h1>'// передает строку с тегами в элемент
-
-
-//создали универсальную функцию для создания элементов
+//создаем универсальную функцию для создания элементов
 const factoryElements = (type, dir, className, text = '') =>{ 
     const elem = document.createElement(type);
+    dir.append(elem);
     elem.classList.add(className)
     elem.innerHTML = text;
-    dir.append(elem);
+    
     return elem;
-}
+};
 
-const btn = factoryElements('button', document.body,'btn', 'Create') 
-
-const container = factoryElements('div', document.body, 'container')
-
+//получаем элементы
+const btn = factoryElements('button', document.body,'btn', 'Create'); 
+const container = factoryElements('div', document.body, 'container');
 const summ = factoryElements('div', document.body, 'total');
-const sec = 10;
 const blockTime =  factoryElements('div', document.body, 'time', sec);
 
-//создали модальное окно
+const sec = 10;
+
+//создаем модальное окно
 const modalVal = `
-    <div class = "modal_window">
+    <div class ="modal_window">
         <h2>Вы набрали: </h2>
-        <div class = "modal_count_points"><h4 class = "point_sum">0</h4><span> очков</span></div> 
+        <div class ="modal_count_points"><h4 class ="point_sum">0 </h4><span>очков</span></div> 
     </div>
 `
 const modal = factoryElements('div', document.body, 'modal-background', modalVal);
@@ -37,21 +30,9 @@ const modal = factoryElements('div', document.body, 'modal-background', modalVal
 const openModal = (elem, points) =>{
     elem.classList.add('opacity');
     elem.querySelector('.point_sum').innerHTML = points.innerHTML;
-}
+};
 
-const createRound = () => {
-    const block =  factoryElements('div', container, 'block');
-    block.style.backgroundColor = '#' + Math.random().toString(16).substr(-6);
-    const blockNum = Math.floor(Math.random()*1000);
-    block.innerHTML =`<p class ='text'>${blockNum }</p>`; 
-
-    block.addEventListener('click', () =>{
-        block.classList.add('scale');
-        summ.innerText = Number(blockNum) + Number(summ.innerText);
-    })
-}
-
-// создали таймер
+// создаем таймер
 const timer = (block, time) =>{
     const interval = setInterval(() => {
         if(block.innerHTML > 0){
@@ -61,14 +42,28 @@ const timer = (block, time) =>{
             openModal(modal, summ);
         }
     }, 1000)
-}
+};
 
+//создаем новые шарики
+const createRound = () => {
+    const block =  factoryElements('div', container, 'block');
+    block.style.backgroundColor = '#' + Math.random().toString(16).substr(-6);
+    const blockNum = Math.floor(Math.random()*1000);
+    block.innerHTML =`<p class ='text'>${blockNum}</p>`; 
+
+    block.addEventListener('click', () =>{
+        block.classList.add('scale');
+        summ.innerText = Number(blockNum) + Number(summ.innerText);
+    })
+};
+
+//запускаем новый раунд
 btn.addEventListener('click', () =>{
     for(let i = 0; i < 60; i++){
         createRound();
     }
     timer(blockTime, sec);
-    btn.disabled = 'true'
-})
+    btn.disabled = 'true';
+});
 
 
